81b0b5ab22c862424cff64801d65691cea44581d
Santu414 <snagankera@gmail.com>
2024-05-16 15:03:57 +0530

    Layout and homepage components

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import '@/assets/styles/globals.css';
+export const metadata = {
+  title: 'PropertyPulse',
+  description: 'Find The Perfect Rental Property',
+};
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
